f23a449e86d34490c594e0478d9e6086d040b842
pramod-kale <pramodkale.sit@gmail.com>
2023-11-26 02:14:11 +0530

    deleted build folder, which was pushed by mistake

--- build/TestComponent/TestComponent.d.ts
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import './TestComponent.scss';
-type Props = {
-    theme: string;
-};
-declare const TestComponent: (props: Props) => React.JSX.Element;
-export default TestComponent;
--- build/index.d.ts
+++
@@ -1,3 +0,0 @@
-import TestComponent from './TestComponent/TestComponent';
-import Pagination from './components/Pagination/Pagination';
-export { TestComponent, Pagination };
